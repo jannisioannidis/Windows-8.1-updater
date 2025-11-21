8f8a7dcb1b410e557177ff5b4fd829a5cb44c7e9
jannisioannidis <244615500+jannisioannidis@users.noreply.github.com>
2025-11-22 00:10:14 +0100

    Added the required cablist, fixed the update.bat file and improved readme
    Former-commit-id: fd22e58dc4584a83d5d088b195569f10566ae4b2

--- README.md
+++ README.md
@@ -4,9 +4,16 @@ This is simply an automatic script that follows this tutorial: https://www.youtu
 
 **I do not take any responsibility if using this script causes harm to your devices**
 
+**Prerequisites**
+To be able to use this script you must have:
+1. The dotnet runtime installed. You can find that here: https://dotnet.microsoft.com/en-us/download
+2. The Windows Phone common packaging tools. You can find their installer here: https://mega.nz/file/7I5GyDTJ#P_-fRVi3oQsfsVecLD_6w5FLH5xhVFDS7AEC9V73qgA
+
 **Usage**
-1. Clone the repo
+1. Clone or download and extract the repo
 
-2. Run the update.bat file
+2. Connect your phone to your computer
 
-3. Open your phone's update page in setttings and install the update!
+3. Run the update.bat file
+
+4. Open your phone's update page in setttings and install the update!
--- Update.bat
+++ Update.bat
@@ -1,18 +1,33 @@
-cd C:\Windows 8.1 updater\1
+#Creates the 1 and 2 folders
+echo Creating 1 and 2 folders...
+mkdir 1
+mkdir 2
+
+#Deletes all previously used InstalledPackages.csv files and filtered files if there were any
+echo Deleting any previously used files
+cd C:\Windows-8.1-updater\1
 del * /S /Q
-cd C:\Windows 8.1 updater\2
+cd C:\Windows-8.1-updater\2
 del * /S /Q
-cd C:\Windows 8.1 updater\WPCabFilter
+cd C:\Windows-8.1-updater\WPCabFilter
 del InstalledPackages.csv /Q
 
+#Creates the DeviceLogs.cab file in the "1" folder which contains the InstalledPackages.csv file
+echo Grabbing DeviceLogs.cab from phone...
 cd C:\Program Files (x86)\Windows Kits\10\Tools\bin\i386
-Getdulogs -o "C:\Windows 8.1 updater\1/DeviceLogs.cab"
+Getdulogs -o "C:\Windows-8.1-updater\1\DeviceLogs.cab"
 
-cd C:\Windows 8.1 updater\1
-expand DeviceLogs.cab -F:InstalledPackages.csv C:\Windows 8.1 updater\WPCabFilter
+#Extracts the InstalledPackages.csv from the DeviceLogs.cab file created in the "1" folder to the 'WPCabFilter' folder
+echo Extracting InstalledPackages.csv...
+cd C:\Windows-8.1-updater\1
+expand DeviceLogs.cab -F:InstalledPackages.csv C:\Windows-8.1-updater\WPCabFilter
 
-cd C:\Windows 8.1 updater\WPCabFilter
-WPCabFilter "C:\Windows 8.1 updater\Cablist" "C:\Windows 8.1 updater\2" 
+#Filters the InstalledPackages.csv files and puts the filtered files in the "2" folder
+echo Filtering files...
+cd C:\Windows-8.1-updater\WPCabFilter
+WPCabFilter "C:\Windows-8.1-updater\Cablist\8.10.15153.165 - BASE" "C:\Windows 8.1 updater\2"
 
+#Pushes the files to your phone
+echo Pushing files to phone...
 cd C:\Program Files (x86)\Windows Kits\10\Tools\bin\i386
-iutool -v -p "C:\Windows 8.1 updater\2"
+iutool -v -p "C:\Windows-8.1-updater\2"
